596c38da61296e53522ff591f40b93ec1411ef98
rvennamaneni161 <richa.vennamaneni@slu.edu>
2025-02-19 01:08:40 +0000

    add edit and destroy orders

--- api.js
+++ api.js
@@ -1,85 +1,35 @@
-const path = require('path')
-const Products = require('./products')
-const autoCatch = require('./lib/auto-catch')
+const path = require('path');
+const Products = require('./products');
+const Orders = require('./orders');
+const autoCatch = require('./lib/auto-catch');
 
 /**
  * Handle the root route
- * @param {object} req
- * @param {object} res
-*/
+ */
 function handleRoot(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.json({ message: "Welcome to Fullstack Prints API!" });
 }
 
 /**
- * List all products
- * @param {object} req
- * @param {object} res
+ * CRUD for Products
  */
-async function listProducts(req, res) {
-  // Extract the limit and offset query parameters
-  const { offset = 0, limit = 25, tag } = req.query
-  // Pass the limit and offset to the Products service
-  res.json(await Products.list({
-    offset: Number(offset),
-    limit: Number(limit),
-    tag
-  }))
-}
-
+async function listProducts(req, res) { res.json(await Products.list(req.query)); }
+async function getProduct(req, res) { res.json(await Products.get(req.params.id)); }
+async function createProduct(req, res) { res.json(await Products.create(req.body)); }
+async function editProduct(req, res) { res.json(await Products.edit(req.params.id, req.body)); }
+async function deleteProduct(req, res) { res.json(await Products.destroy(req.params.id)); }
 
 /**
- * Get a single product
- * @param {object} req
- * @param {object} res
+ * CRUD for Orders
  */
-async function getProduct(req, res, next) {
-  const { id } = req.params
-
-  const product = await Products.get(id)
-  if (!product) {
-    return next()
-  }
-
-  return res.json(product)
-}
-
-/**
- * Create a product
- * @param {object} req 
- * @param {object} res 
- */
-async function createProduct(req, res) {
-  console.log('request body:', req.body)
-  res.json(req.body)
-}
-
-/**
- * Edit a product
- * @param {object} req
- * @param {object} res
- * @param {function} next
- */
-async function editProduct(req, res, next) {
-  console.log(req.body)
-  res.json(req.body)
-}
-
-/**
- * Delete a product
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
- */
-async function deleteProduct(req, res, next) {
-  res.json({ success: true })
-}
+async function listOrders(req, res) { res.json(await Orders.list(req.query)); }
+async function getOrder(req, res) { res.json(await Orders.get(req.params.id)); }
+async function createOrder(req, res) { res.json(await Orders.create(req.body)); }
+async function editOrder(req, res) { res.json(await Orders.edit(req.params.id, req.body)); }
+async function deleteOrder(req, res) { res.json(await Orders.destroy(req.params.id)); }
 
 module.exports = autoCatch({
   handleRoot,
-  listProducts,
-  getProduct,
-  createProduct,
-  editProduct,
-  deleteProduct
+  listProducts, getProduct, createProduct, editProduct, deleteProduct,
+  listOrders, getOrder, createOrder, editOrder, deleteOrder
 });
--- script/import-products.js
+++ script/import-products.js
@@ -1,11 +1,13 @@
 const db = require('../db');
 const Products = require('../products');
-
 const products = require('../data/full-products.json');
 
-;(async () => {
-  // loop over the products and create them
+(async () => {
   for (let i = 0; i < products.length; i++) {
-    console.log( await Products.create(products[i]));
+    try {
+      console.log(await Products.create(products[i]));
+    } catch (error) {
+      console.error('❌ Error importing product:', error.message);
+    }
   }
-})()
+})();
